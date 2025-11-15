2785008c4f8fbfb892f5bc82cf83c045535a4641
Deepak K <koravangattu.deepak@gmail.com>
2025-11-15 14:51:46 +0530

    first commit

---
+++ mcp-crash-course-project-langchain-mcp-adapters/main.py
@@ -0,0 +1,44 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+# ClientSession will provide an framwork to act as a MCP Client
+# StdioServerParameters will provide the parameters to start a MCP server
+from mcp.client.stdio import stdio_client
+# stdio_client is going to communicate to the MCP server via transport layer of stdio, means it going to read from standatd input and write to standard output
+# thats how client is going to communicate with the MCP server
+
+load_dotenv()
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(model="gemma2-9b-it", temperature=0)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["D:/Projects/MCPs/mcp-servers/mcp-crash-course-project-langchain-mcp-adapters/servers/math_server.py"],
+)
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read,write):    
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+
+            print(f"tools: {tools}")
+
+            agent = create_react_agent(llm,tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
